5ae7b08ede65c51d20c8c4d774c22dc31b77e0b5
lucapu88 <caputoluca88@gmail.com>
2020-02-18 17:52:55 +0100

    creazione layout (homepage pubblica, homepage admin ecc..)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,5 @@
-@extends('layouts.app')
-
+{{-- HOMEPAGE PUBLICA --}}
+@extends('layouts.public')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+  <img src="https://images.pexels.com/photos/546819/pexels-photo-546819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="sfondo_code_home" width="100%">
 @endsection
--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,30 +1,24 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-    <div id="app">
-      @include('layouts.partials.navbar')
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
-</body>
+  </head>
+  <body>
+    <main>
+      @include('layouts.partials.public-navbar')
+      @yield('content')
+    </main>
+  </body>
 </html>
