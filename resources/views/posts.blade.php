f23ceb788d85f2195702b2e2d72e79565962c65b
lucapu88 <caputoluca88@gmail.com>
2020-02-19 09:12:51 +0100

    inserimento e visualizzazione posts in homepage

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,19 +4,28 @@
     <div class="row">
       <div class="col-sm-12">
         <h1>Lista post</h1>
-        <div class="card-body">
+        <ul>
           @forelse ($posts as $post)
-
+            <li>{{$post->title}}</li>
           @empty
-            <p class="card-text">Non ci sono ancora post</p>
+          <li>Non ci sono ancora post</li>
           @endforelse
-          <h2 class="card-title">{{$phone->name}}</h2>
-          <h6 class="card-subtitle mb-2 text-muted">Phone ID: {{$phone->id}}</h6>
-          <p class="card-text"><strong>Prezzo €: </strong> {{$phone->price}}</p>
-          <p class="card-text"><strong>Colore: </strong> {{$phone->color}}</p>
-          <p class="card-text"><strong>Creato il: </strong> {{$phone->created_at}}</p>
-          <p class="card-text"><strong>Aggiornato il: </strong> {{$phone->updated_at}}</p>
-        </div>
+        </ul>
+
+        {{-- <div class="card" style="width: 30rem;">
+          <div class="card-body">
+            @forelse ($posts as $post)
+              <h2 class="card-title">{{$post->title}}</h2>
+              <h6 class="card-subtitle mb-2 text-muted">ID: {{$post->id}}</h6>
+              <p class="card-text"><strong>Slug: </strong> {{$post->slug}}</p>
+              <p class="card-text"><strong>Autore: </strong> {{$post->author}}</p>
+              <p class="card-text"><strong>Creato il: </strong> {{$post->created_at}}</p>
+              <p class="card-text"><strong>Aggiornato il: </strong> {{$post->updated_at}}</p>
+            @empty
+              <p class="card-text">Non ci sono ancora post</p>
+            @endforelse
+          </div>
+        </div> --}}
       </div>
     </div>
   </div>
